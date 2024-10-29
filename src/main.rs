e2e24044a826fa63255d1d88aba619424799ac9c
Alexander Dernild <alex@dernild.dk>
2024-10-29 22:28:29 +0100

    Adding a random special surprise

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use rand::seq::SliceRandom;
 use serde::Serialize;
 use std::sync::Mutex;
 
@@ -6,6 +7,8 @@ use std::sync::Mutex;
 #[derive(Serialize)]
 struct CakeResponse {
     message: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    surprise: Option<String>,
     slices_left: i32,
 }
 
@@ -14,20 +17,45 @@ struct AppState {
     cake_slices: Mutex<i32>,
 }
 
+// Function to get random surprise message with 50% chance
+fn get_random_surprise() -> Option<String> {
+    if rand::random::<bool>() {
+        // 50% chance of getting a surprise
+        let surprises = vec![
+            "Lucky you, you got a strawberry!",
+            "There's a cherry on top!",
+            "Extra chocolate sprinkles for you!",
+            "This slice has rainbow frosting!",
+            "You found a hidden candy in your slice!",
+        ];
+        Some(
+            surprises
+                .choose(&mut rand::thread_rng())
+                .unwrap_or(&"Enjoy your cake!")
+                .to_string(),
+        )
+    } else {
+        None
+    }
+}
+
 // Handler for the POST request
 async fn get_cake(data: web::Data<AppState>) -> impl Responder {
     let mut slices = data.cake_slices.lock().unwrap();
+    let surprise = get_random_surprise();
 
     let response = if *slices > 0 {
         // Decrease the number of slices
         *slices -= 1;
         CakeResponse {
             message: "Here you are, enjoy this slice of cake!".to_string(),
+            surprise,
             slices_left: *slices,
         }
     } else {
         CakeResponse {
             message: "Unfortunately there is no more cake, have a hug instead 🤗".to_string(),
+            surprise: None,
             slices_left: 0,
         }
     };
