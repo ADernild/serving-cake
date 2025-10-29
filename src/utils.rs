61fcae785699573ab5c7e28fff22e2a794e17a1e
ADernild <alex@dernild.dk>
2025-10-29 23:10:43 +0100

    more surprises

--- src/utils.rs
+++ src/utils.rs
@@ -4,11 +4,13 @@ pub fn get_random_surprise() -> Option<String> {
     if rand::random::<bool>() {
         // 50% chance of getting a surprise
         let surprises = vec![
-            "Lucky you, you got a strawberry!",
-            "There's a cherry on top!",
-            "Extra chocolate sprinkles for you!",
-            "This slice has rainbow frosting!",
-            "You found a hidden candy in your slice!",
+            "Lucky you, you got a strawberry! 🍓",
+            "There's a cherry on top! 🍒",
+            "Extra chocolate sprinkles for you! 🍫",
+            "This slice has rainbow frosting! 🌈",
+            "You found a hidden candy in your slice! 🍬",
+            "Candied bacon YUM! 🥓",
+            "Eew a severed toe! 🩸"
         ];
         Some(
             surprises
